014c20efa7afe3f97a76ee4fc054f10a7dd0364f
Saket Thapliyal <saket.thapliyal@aepl.com>
2016-06-28 17:36:59 +0530

    Saket Thapliyal

---
+++ C#/Saket Thapliyal/CS_Assignment/CS_Assignment/Calc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc_lib
+{
+    internal interface ICalculator<T>
+    {
+        T Add(T _val1, T _val2);
+        T Sub(T _val1, T _val2);
+        T Mul(T _val1, T _val2);
+        T Div(T _val1, T _val2);
+    }
+}
+    
+
+    
+
+       
+
+    
+

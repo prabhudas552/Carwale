210eef829cd3955d7cd257a0e152a6d3fd8177f6
Shubham Agarwal <shubham.agarwal@aepl.com>
2016-06-28 17:41:36 +0530

    C# Assignment - Shubham Agarwal

---
+++ C#/Shubham Agarwal/CalculatorLibrary/CalculatorLibrary/BinaryCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class BinaryCalculator : ICalculator<string>
+    {
+
+        public string Add(string a, string b)
+        {
+            int x = Convert.ToInt32(a, 2);
+            int y = Convert.ToInt32(b, 2);
+            return Convert.ToString(x + y, 2);
+        }
+
+        public string Subtract(string a, string b)
+        {
+            int x = Convert.ToInt32(a, 2);
+            int y = Convert.ToInt32(b, 2);
+            return Convert.ToString(x - y, 2);
+        }
+
+        public string Multiply(string a, string b)
+        {
+            int x = Convert.ToInt32(a, 2);
+            int y = Convert.ToInt32(b, 2);
+            return Convert.ToString(x * y, 2);
+        }
+
+        public string Divide(string a, string b)
+        {
+            int x = Convert.ToInt32(a, 2);
+            int y = Convert.ToInt32(b, 2);
+            return Convert.ToString(x / y, 2);
+        }
+    }
+}

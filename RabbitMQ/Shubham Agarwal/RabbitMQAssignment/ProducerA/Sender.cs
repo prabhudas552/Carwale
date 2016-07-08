40ab5e3e7416088ecd2763da4583d7b183110b67
Shubham Agarwal <shubham.agarwal@aepl.com>
2016-07-08 15:16:47 +0530

    RabbitMQ Assignment Added.

---
+++ RabbitMQ/Shubham Agarwal/RabbitMQAssignment/ProducerA/Sender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace ProducerA
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Sender-A");
+            var factory = new ConnectionFactory() { HostName = "172.16.8.3" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "sender", type: ExchangeType.Direct);
+
+                string message = "Hello";
+                Console.WriteLine(" Write \'exit\' to exit.");
+                while (!message.Equals("exit"))
+                {
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "sender", routingKey: "ProducerA", basicProperties: null, body: body);
+                    Console.WriteLine("Sent: '{0}'", message);
+                    message = Console.ReadLine();
+                }
+            }
+        }
+    }
+}

7792d0d28db9f80c25bffb8c443f2ec5ae9de9bf
Ravi Teja <ravi.teja@aepl.com>
2016-07-08 15:10:09 +0530

    Ravi Teja Submission
    RabbitMQ submission by Ravi Teja

---
+++ RabbitMQ/Ravi Teja/RabbitMQ_Ravi/Messi.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ_Ravi
+{
+    public class Messi
+    {
+        public void StartConnection()
+        {
+            var factory = new ConnectionFactory() { HostName = "172.16.8.3"};
+            //factory.Uri = @"amqp://kftlmyix:H4wyAFjNMmxSjZ0_DFrEEwpEnaqdgE1V@wildboar.rmq.cloudamqp.com/kftlmyix";
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    string routingKey = "messi";
+                    channel.ExchangeDeclare(exchange: "ExchangeMessi",
+                                        type: "topic");
+                    channel.QueueDeclare(queue: "Queue_FCB",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+                    Console.WriteLine("Enter the message...");
+                    while (true)
+                    {
+                        Console.Write(">> ");
+                        string message = Console.ReadLine();
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "ExchangeMessi",
+                                             routingKey: routingKey,
+                                             basicProperties: null,
+                                             body: body);
+                        Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+                        if (message.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                            break;
+                    }
+                    Console.WriteLine("Adiós..!!\n Press ENTER to exit.");
+                    Console.ReadKey();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Messi..!!\n Press ENTER to begin...");
+            Console.ReadLine();
+            new Messi().StartConnection();
+        }
+    }
+
+}
+
+
---
+++ RabbitMQ/Ravi Teja/UserTeja/Teja.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace UserTeja
+{
+    public class Teja
+    {
+        public void ReadMessage()
+        {
+            var factory = new ConnectionFactory() { HostName = "172.16.8.3" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    channel.QueueBind(queue: queueName,
+                                     exchange: "ExchangeMessi",
+                                     routingKey: "messi");
+
+                    Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                    while (true)
+                    {
+                        var consumer = new EventingBasicConsumer(channel);
+                        consumer.Received += (model, ea) =>
+                        {
+                            var body = ea.Body;
+                            var message = Encoding.UTF8.GetString(body);
+                            if (message.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                message = "Went Offline";
+                            }
+                            var routingKey = ea.RoutingKey;
+                            Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                              routingKey,
+                                              message);
+                        };
+                        channel.BasicConsume(queue: queueName,
+                                             noAck: true,
+                                             consumer: consumer);
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Teja..!!\n Press ENTER to begin...");
+            Console.ReadLine();
+            new Teja().ReadMessage();
+        }
+
+    }
+}

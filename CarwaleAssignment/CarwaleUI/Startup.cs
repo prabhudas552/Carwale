d2d041eb2ab133cfde0b374a5b5dd07983ee7a97
prabhudas552 <garule.prabhudas@carwale.com>
2016-07-11 13:48:35 +0530

    Added..!

---
+++ CarwaleAssignment/CarwaleUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarwaleUI.Startup))]
+namespace CarwaleUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
